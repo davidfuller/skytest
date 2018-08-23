76a017cd84adbe061a595c10c0ce136f02eba540
David Fuller <david@muvi2.co.uk>
2018-08-23 18:40:55 +0100

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   resources :titles
   resources :users
   root 'users#index'
+  
+  resources :titles do
+  	member do
+  		get 'add_episode'
+  	end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
