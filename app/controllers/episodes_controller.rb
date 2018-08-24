7d58e5e25fb5d2e5d8be9c92be907c9985300815
David Fuller <david@muvi2.co.uk>
2018-08-24 10:49:32 +0100

    Added episode_title to allowed params

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -69,6 +69,6 @@ class EpisodesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def episode_params
-      params.require(:episode).permit(:year, :season, :episode, :title_id)
+      params.require(:episode).permit(:year, :season, :episode, :title_id, :episode_title)
     end
 end
