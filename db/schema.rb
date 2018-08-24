f96307f3c44376694821ebef8fe02644d6d277d7
David Fuller <david@muvi2.co.uk>
2018-08-24 09:38:35 +0100

    Added episode title

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180821085607) do
+ActiveRecord::Schema.define(version: 20180824083748) do
 
   create_table "episodes", force: :cascade do |t|
-    t.string   "year",       limit: 255
-    t.string   "season",     limit: 255
-    t.string   "episode",    limit: 255
-    t.integer  "title_id",   limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "year",          limit: 255
+    t.string   "season",        limit: 255
+    t.string   "episode",       limit: 255
+    t.integer  "title_id",      limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.string   "episode_title", limit: 255
   end
 
   add_index "episodes", ["title_id"], name: "index_episodes_on_title_id", using: :btree
