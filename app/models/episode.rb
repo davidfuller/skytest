f6f3fb653bb56640dcad3ad0841cbca9f7108a7f
David Fuller <david@muvi2.co.uk>
2018-08-24 17:04:46 +0100

    Numericise take 1

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,9 +1,15 @@
 class Episode < ActiveRecord::Base
   belongs_to :title
   
-  default_scope { order(:season.to_i).order(:episode).order(:year) }
+  default_scope { order(:int_season).order(:int_episode).order(:int_year) }
   
   attr_accessor :source
   
   
+  def numericise
+  	int_year = year.to_i
+  	int_season = season.to_i
+  	int_episode = spisode.to_i
+  end
+      
 end
