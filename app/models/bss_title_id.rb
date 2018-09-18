01a848e4fc836389a0d496bf498fd4a8c242c5d4
David Fuller <david@muvi2.co.uk>
2018-09-18 07:31:45 +0100

    Listing BSS Title ID stuff

--- app/models/bss_title_id.rb
+++ app/models/bss_title_id.rb
@@ -1,7 +1,19 @@
 class BssTitleId < ActiveRecord::Base
 
-    belongs_to :episode
+  belongs_to :episode
 
-    attr_accessor :source
+  validates_presence_of :bss_title_id
+  default_scope { order(:bss_title_id) }
+  
+  attr_accessor :source
 
+	self.per_page = 12
+	
+	def self.search(search, page)
+		if search
+			where('bss_title_id LIKE ?', "%#{search}%").paginate(page: page)
+		else
+			paginate	:page => page
+		end
+	end
 end
