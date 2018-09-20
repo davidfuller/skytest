514349ed15f5d2663f721a314c70d5e1a2b836b2
David Fuller <david@muvi2.co.uk>
2018-09-20 09:07:56 +0100

    migration

--- db/migrate/20180920080158_create_bss_channel_joins.rb
+++ db/migrate/20180920080158_create_bss_channel_joins.rb
@@ -1,7 +1,9 @@
 class CreateBssChannelJoins < ActiveRecord::Migration
   def change
     create_table :bss_channel_joins do |t|
-
+      t.integer :channel_id
+      t.integer :bss_title_id_id
+      
       t.timestamps null: false
     end
   end
