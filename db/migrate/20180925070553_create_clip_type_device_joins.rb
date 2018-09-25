45c12392f5417ca5d5c738a67fa6a35886d45744
David Fuller <david@muvi2.co.uk>
2018-09-25 08:08:43 +0100

    Added migration for Clip Type Device join

--- db/migrate/20180925070553_create_clip_type_device_joins.rb
+++ db/migrate/20180925070553_create_clip_type_device_joins.rb
@@ -1,7 +1,8 @@
 class CreateClipTypeDeviceJoins < ActiveRecord::Migration
   def change
     create_table :clip_type_device_joins do |t|
-
+      t.integer :clip_type_id
+      t.integer :device_type_id
       t.timestamps null: false
     end
   end
