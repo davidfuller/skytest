74d6b03afda835a2d948fc29b3d8e1a5f2835667
David Fuller <david@muvi2.co.uk>
2018-09-25 18:10:15 +0100

    Sort promo type too

--- app/models/clip_type.rb
+++ app/models/clip_type.rb
@@ -31,7 +31,7 @@ class ClipType < ActiveRecord::Base
     clip_type_channel_joins.where('tx = ?', true).joins(:channel).order('channels.name')
   end
   def promo_channels
-    clip_type_channel_joins.where('tx = ?', false)
+    clip_type_channel_joins.where('tx = ?', false).joins(:channel).order('channels.name')
   end
 
   def delete_channel(channel, tx)
