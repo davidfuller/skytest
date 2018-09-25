207eaed574da81e475edf7165ba4914c6f2ef260
David Fuller <david@muvi2.co.uk>
2018-09-25 09:22:42 +0100

    Some display changes

--- app/models/clip_type.rb
+++ app/models/clip_type.rb
@@ -3,6 +3,15 @@ class ClipType < ActiveRecord::Base
   default_scope { order(:name) }
 	
 	validates_uniqueness_of :name, :messsage => " is already in system"
-	validates_presence_of :name
+  validates_presence_of :name
+  
+    def default_has_audio_display
+    if default_has_audio
+      'Has Audio'
+    else
+      ''
+    end
+  end
+	
 
 end
--- app/models/device_type.rb
+++ app/models/device_type.rb
@@ -3,6 +3,6 @@ class DeviceType < ActiveRecord::Base
   default_scope { order(:name) }
 	
 	validates_uniqueness_of :name, :messsage => " is already in system"
-	validates_presence_of :name
-	
+  validates_presence_of :name
+    
 end
