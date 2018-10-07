caa6e85096606470b3b6deb1bf5da7c6cbb97df9
David Fuller <davidfuller@users.noreply.github.com>
2018-10-07 21:00:28 +0100

    Typo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,7 @@ module ApplicationHelper
   end
   
   def add_device(source, clip_type, device, params)
-    my_params = {id: @clip_type, device_id: device, promo_data_show: params[:promo_data_show], tx_data_show: params[:tx_data_show], search: params[:search], channel_add_show: params[:channel_add_show]}
+    my_params = {id: clip_type, device_id: device, promo_data_show: params[:promo_data_show], tx_data_show: params[:tx_data_show], search: params[:search], channel_add_show: params[:channel_add_show]}
     
     if source == :clip_type
       add_device_type_clip_type_path(my_params)
