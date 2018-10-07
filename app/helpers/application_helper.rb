8d9a050268f8b516264b61d3cc28d11a6b1a6547
David Fuller <davidfuller@users.noreply.github.com>
2018-10-07 15:30:29 +0100

    Trying out a hash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,8 +6,10 @@ module ApplicationHelper
 
 
   def remove_device(source, clip_type, device, params)
+    my_params = {id: clip_type, device_id: device, promo_data_show: params[:promo_data_show], tx_data_show: params[:tx_data_show], search: params[:search], channel_add_show: params[:channel_add_show]}
+    
     if source == :clip_type
-      remove_device_type_clip_type_path(id: clip_type, device_id: device, promo_data_show: params[:promo_data_show], tx_data_show: params[:tx_data_show], search: params[:search], channel_add_show: params[:channel_add_show])
+      remove_device_type_clip_type_path(my_params)
     end
   end
 end
