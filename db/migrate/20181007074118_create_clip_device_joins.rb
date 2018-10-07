e103fde18bc9ab352e3182aa804a81f03da4652b
David Fuller <davidfuller@users.noreply.github.com>
2018-10-07 10:18:01 +0100

    typo removed

--- db/migrate/20181007074118_create_clip_device_joins.rb
+++ db/migrate/20181007074118_create_clip_device_joins.rb
@@ -2,7 +2,7 @@ class CreateClipDeviceJoins < ActiveRecord::Migration
   def change
     create_table :clip_device_joins do |t|
       t.integer :clip_id
-      t.integer :device_typed_id
+      t.integer :device_type_id
 
       t.timestamps null: false
     end
