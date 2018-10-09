f65a7bbfeee5a0b737f2d866fed05283d3c29bd2
David Fuller <david@muvi2.co.uk>
2018-10-09 17:08:13 +0100

    Let's try this

--- app/helpers/clips_helper.rb
+++ app/helpers/clips_helper.rb
@@ -6,13 +6,15 @@ module ClipsHelper
 
 
   def remove_bss(clip, bss, params)
-    my_params = {id: clip, bss_id: bss, promo_data_show: params[:promo_data_show], tx_data_show: params[:tx_data_show], search: params[:search], channel_add_show: params[:channel_add_show]}
+    my_params = {id: clip, bss_id: bss, promo_data_show: params[:promo_data_show], tx_data_show: params[:tx_data_show], search: params[:search], 
+                  channel_add_show: params[:channel_add_show], search_bss: params[:search_bss]}
     
     remove_bss_clip_path(my_params)
   end
   
   def add_bss(clip, bss, params)
-    my_params = {id: clip, bss_id: bss, promo_data_show: params[:promo_data_show], tx_data_show: params[:tx_data_show], search: params[:search], channel_add_show: params[:channel_add_show]}
+    my_params = {id: clip, bss_id: bss, promo_data_show: params[:promo_data_show], tx_data_show: params[:tx_data_show], search: params[:search], 
+                  channel_add_show: params[:channel_add_show], search_bss: params[:search_bss]}
     
     add_bss_clip_path(my_params)
   end
