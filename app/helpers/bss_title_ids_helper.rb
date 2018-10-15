cbc746d78bb3a7bb1d273f5e42a83b817a1ed178
David Fuller <davidfuller@users.noreply.github.com>
2018-10-15 22:35:20 +0100

    Better channel display in bss show

--- app/helpers/bss_title_ids_helper.rb
+++ app/helpers/bss_title_ids_helper.rb
@@ -1,2 +1,8 @@
 module BssTitleIdsHelper
+
+  def remove_channel(bss, channel, params)
+    my_params = {id: bss, channel_id: channel, search: params[:search], channel_data_show: params[:channel_data_show], channel_add_show: params[:channel_add_show], 
+                  clip_data_show: params[:clip_data_show], clip_add_show: params[:clip_add_show], search_clip: params[:search_clip]}
+    remove_channel_bss_title_id_path(my_params)
+  end
 end
