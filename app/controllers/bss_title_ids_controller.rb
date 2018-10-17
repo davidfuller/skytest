a7d2c57db10552970494eda3d7cdb4473bc19190
David Fuller <davidfuller@users.noreply.github.com>
2018-10-17 22:08:30 +0100

    Added @clips in bss controller

--- app/controllers/bss_title_ids_controller.rb
+++ app/controllers/bss_title_ids_controller.rb
@@ -14,6 +14,7 @@ class BssTitleIdsController < ApplicationController
   # GET /bss_title_ids/1.json
   def show
     @channels = Channel.bss_title_id_search(params[:search])
+    @clips = Clip.all
   end
 
   # GET /bss_title_ids/new
