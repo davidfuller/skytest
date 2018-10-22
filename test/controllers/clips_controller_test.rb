f28c5eca3f7279688f020ad617c6bdc53a11836d
David Fuller <davidfuller@users.noreply.github.com>
2018-10-22 22:53:31 +0200

    bss fixes

--- test/controllers/clips_controller_test.rb
+++ test/controllers/clips_controller_test.rb
@@ -17,8 +17,9 @@ class ClipsControllerTest < ActionController::TestCase
   end
 
   test "should create clip" do
+    bss = BssTitleIds(:one)
     assert_difference('Clip.count') do
-      post :create, clip: { audio_filename: @clip.audio_filename, clip_type_id: @clip.clip_type_id, completion: @clip.completion, duration: @clip.duration, end_episode: @clip.end_episode, end_season: @clip.end_season, filename: @clip.filename, first_use: @clip.first_use, folder_id: @clip.folder_id, has_audio: @clip.has_audio, last_use: @clip.last_use, name: @clip.name, note: @clip.note, season_generic: @clip.season_generic, start_episode: @clip.start_episode, start_season: @clip.start_season, status_id: @clip.status_id, totally_generic: @clip.totally_generic, user_id: @clip.user_id }
+      post :create, clip: { audio_filename: @clip.audio_filename, clip_type_id: @clip.clip_type_id, completion: @clip.completion, duration: @clip.duration, end_episode: @clip.end_episode, end_season: @clip.end_season, filename: @clip.filename, first_use: @clip.first_use, folder_id: @clip.folder_id, has_audio: @clip.has_audio, last_use: @clip.last_use, name: @clip.name, note: @clip.note, season_generic: @clip.season_generic, start_episode: @clip.start_episode, start_season: @clip.start_season, status_id: @clip.status_id, totally_generic: @clip.totally_generic, user_id: @clip.user_id, dummy_bss_id: bss}
     end
 
     assert_redirected_to clips_path
@@ -36,7 +37,7 @@ class ClipsControllerTest < ActionController::TestCase
 
   test "should update clip" do
     patch :update, id: @clip, clip: { audio_filename: @clip.audio_filename, clip_type_id: @clip.clip_type_id, completion: @clip.completion, duration: @clip.duration, end_episode: @clip.end_episode, end_season: @clip.end_season, filename: @clip.filename, first_use: @clip.first_use, folder_id: @clip.folder_id, has_audio: @clip.has_audio, last_use: @clip.last_use, name: @clip.name, note: @clip.note, season_generic: @clip.season_generic, start_episode: @clip.start_episode, start_season: @clip.start_season, status_id: @clip.status_id, totally_generic: @clip.totally_generic, user_id: @clip.user_id }
-    assert_redirected_to clip_path(assigns(:clip))
+    assert_redirected_to clips_path
   end
 
   test "should destroy clip" do
