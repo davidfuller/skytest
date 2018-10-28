81632370ce41e6a8fcc0c318b82b473b168c82fd
David Fuller <davidfuller@users.noreply.github.com>
2018-10-28 16:10:23 +0000

    Trying to fix migration 2

--- db/migrate/20181028153723_add_devise_to_users.rb
+++ db/migrate/20181028153723_add_devise_to_users.rb
@@ -46,6 +46,10 @@ class AddDeviseToUsers < ActiveRecord::Migration
   def self.down
     # By default, we don't want to make any assumption about how to roll back a migration when your
     # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
+    remove_column :users, :email
+    remove_column :users, :encrypted_password
+    remove_column :users, :reset_password_token
+    remove_column :users, :reset_password_sent_at
+  
   end
 end
