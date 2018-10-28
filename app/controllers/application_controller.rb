80a664bd4f3b3c122306850e311b592c69a15cc9
David Fuller <davidfuller@users.noreply.github.com>
2018-10-28 18:43:02 +0000

    Trying to do a registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
   
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:name, :role)
+    end
+    
+  end
+  
 end
