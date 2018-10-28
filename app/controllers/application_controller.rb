dde300819c322d2d6858d375ea36661f19a79f2f
David Fuller <davidfuller@users.noreply.github.com>
2018-10-28 23:24:47 +0000

    This should work for sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     
-    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit(:name, :role)
     end
     
