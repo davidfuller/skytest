df14797ee0aeff4151a97578e2b10b0b6501efff
David Fuller <davidfuller@users.noreply.github.com>
2018-10-28 16:39:40 +0000

    Hopefully final devise migration

--- db/migrate/20181028153723_add_devise_to_users.rb
+++ db/migrate/20181028153723_add_devise_to_users.rb
@@ -12,35 +12,35 @@ class AddDeviseToUsers < ActiveRecord::Migration
       t.datetime :reset_password_sent_at
 
       ## Rememberable
-      #t.datetime :remember_created_at
+      t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
+      t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      t.string   :unlock_token # Only if unlock strategy is :email or :both
+      t.datetime :locked_at
 
 
       # Uncomment below if timestamps were not included in your original model.
       # t.timestamps null: false
     end
 
-    # add_index :users, :email,                unique: true
-    # add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    add_index :users, :confirmation_token,   unique: true
+    add_index :users, :unlock_token,         unique: true
   end
 
   def self.down
@@ -50,6 +50,28 @@ class AddDeviseToUsers < ActiveRecord::Migration
     remove_column :users, :encrypted_password
     remove_column :users, :reset_password_token
     remove_column :users, :reset_password_sent_at
+  
+    remove_column :users, :remember_created_at
+
+    remove_column :users, :sign_in_count, default: 0, null: false
+    remove_column :users, :current_sign_in_at
+    remove_column :users, :last_sign_in_at
+    remove_column :users, :current_sign_in_ip
+    remove_column :users, :last_sign_in_ip
+
+    remove_column :users, :confirmation_token
+    remove_column :users, :confirmed_at
+    remove_column :users, :confirmation_sent_at
+    remove_column :users, :unconfirmed_email
+    
+    remove_column :users, :failed_attempts
+    remove_column :users, :unlock_token # Only if unlock strategy is :email or :both
+    remove_column :users, :locked_at
+
+    remove_index :users, column: :email
+    remove_index :users, column: :reset_password_token
+    remove_index :users, column: :confirmation_token
+    remove_index :users, column: :unlock_token
   
   end
 end
