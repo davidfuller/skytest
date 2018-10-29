69f8f3d31ffe46cf7b693a520a64dcb2ab57856f
David Fuller <david@muvi2.co.uk>
2018-10-29 11:02:57 +0000

    Put routes back

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path_names: { registration: "registrations" } 
+  devise_for :users
   resources :clips
   resources :statuses
   resources :folders
