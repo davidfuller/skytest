5ef159693aaebb524fb7de2cf65a22913828d322
David Fuller <davidfuller@users.noreply.github.com>
2018-10-29 13:53:41 +0000

    Update  account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
       user_params.permit(:name, :role, :email, :password)
     end
     
-    devise_parameter_sanitizer.permit(:edit) do |user_params|
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
       user_params.permit(:name, :role, :email, :password)
     end
     
