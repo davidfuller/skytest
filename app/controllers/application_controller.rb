03ae554f5a9d708e35eaa6a51ae711a945da5da5
David Fuller <david@muvi2.co.uk>
2018-10-29 10:09:54 +0000

    Added stuff to go back after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,12 @@ class ApplicationController < ActionController::Base
   
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # saves the location before loading each page so we can return to the
+  # right page. If we're on a devise page, we don't want to store that as the
+  # place to return to (for example, we don't want to return to the sign in page
+  # after signing in), which is what the :unless prevents
+  before_filter :store_current_location, unless: :devise_controller?
+
   protected
 
   def configure_permitted_parameters
@@ -15,5 +21,13 @@ class ApplicationController < ActionController::Base
     end
     
   end
-  
+
+  private
+  # override the devise helper to store the current location so we can
+  # redirect to it after loggin in or out. This override makes signing in
+  # and signing up work automatically.
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+
 end
