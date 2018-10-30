710b220f10bf1991fc161635d5a6b9dcfb64f86b
David Fuller <david@muvi2.co.uk>
2018-10-30 10:39:56 +0000

    Included password confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :role, :email, :password, :current_password)
+      params.require(:user).permit(:name, :role, :email, :password, :password_confirmation, :current_password)
     end
 end
