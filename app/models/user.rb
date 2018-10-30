3f147d8f6bd41ef7838294c95b7be5cc5287d664
David Fuller <davidfuller@users.noreply.github.com>
2018-10-30 20:18:45 +0000

    Made my update

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,16 @@ class User < ActiveRecord::Base
 	validates_uniqueness_of :name, :messsage => " is already in system"
 	validates_presence_of :name, :role
 	
+	
+	def my_update(params, *options)
+
+    if params[:password].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
