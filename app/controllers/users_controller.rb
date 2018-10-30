54ccc05e0f9bf74a37e0c68d315c38229c3c3aa8
David Fuller <davidfuller@users.noreply.github.com>
2018-10-30 19:37:45 +0000

    Added admin to user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :role, :email, :password, :password_confirmation, :current_password)
+      params.require(:user).permit(:name, :role, :email, :admin, :password, :password_confirmation, :current_password)
     end
 end
