7f13c87c962c2f82a3faba793928bbd754e6d465
David Fuller <david@muvi2.co.uk>
2018-12-04 14:41:50 +0000

    Fixing the routing

--- app/helpers/bss_title_ids_helper.rb
+++ app/helpers/bss_title_ids_helper.rb
@@ -28,7 +28,8 @@ module BssTitleIdsHelper
   
   def my_params_bss(bss, params)
     {id: bss, search: params[:search], channel_data_show: params[:channel_data_show], channel_add_show: params[:channel_add_show], 
-                  clip_data_show: params[:clip_data_show], clip_add_show: params[:clip_add_show], search_clip: params[:search_clip]}
+                  clip_data_show: params[:clip_data_show], clip_add_show: params[:clip_add_show], search_clip: params[:search_clip], 
+                  source: params[:source]}
   end
     
   
